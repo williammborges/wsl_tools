8a1ac90f878e57a031b4e98396f05d8d5d48f56d
William Borges <williammrborges@protonmail.com>
2023-08-12 13:50:02 -0300

    shell to install tools

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt dist-upgrade
+
+#Tools
+sudo apt install git john hashcat netcat-traditional hash-identifier dirb wordlists wpscan patator wfuzz ffuf hydra metasploit-framework openvpn gobuster dirbuster dirb nmap nikto sqlmap nuclei wpscan steghide dnsenum sublist3r assetfinder mimikatz responder whatweb theharvester amass exploitdb enum4linux tcpdump feroxbuster naabu wafw00f python3-impacket impacket-scripts crackmapexec curl evil-winrm hashid nasm net-tools seclists whois
